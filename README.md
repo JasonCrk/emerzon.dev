075683ed07cb265eb1c2161f41aa9b6ea111b0c5
JasonCrk <emerzonkolki@gmail.com>
2024-07-24 13:15:36 -0500

    feat: v1

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center">
+    <h2 align="center">Emerzon Javier Kolki Martinez</h2>
+</p>
---
+++ src/i18n/ui.ts
@@ -0,0 +1,35 @@
+export const languages = {
+    es: 'Spanish',
+    en: 'English',
+}
+
+export const defaultLang = 'es'
+
+export const ui = {
+    es: {
+        'nav.home': 'Inicio',
+        'nav.projects': 'Proyectos',
+        'nav.skills': 'Conocimientos',
+        'nav.aboutMe': 'Sobre mi',
+        'hero-author.greeting': 'Hola, soy',
+        'work-status.working': 'Actualmente trabajando',
+        'work-status.job-search': 'Disponible para trabajar',
+        'hero-author.role': 'Desarrollador Backend',
+        'hero-author.location': 'Soy de Trujillo, La Libertad, Perú 🇵🇪.',
+        'hero-author.funfact': 'Autodidacta desde los 14 años',
+        'hero-links.contact': 'Contactame',
+    },
+    en: {
+        'nav.home': 'Home',
+        'nav.projects': 'Projects',
+        'nav.skills': 'Skills',
+        'nav.aboutMe': 'About me',
+        'hero-author.greeting': "Hey, I'm",
+        'work-status.working': 'Currently Working',
+        'work-status.job-search': 'Available for work',
+        'hero-author.role': 'Backend Developer',
+        'hero-author.location': "I'm from Trujillo, La Libertad, Peru 🇵🇪.",
+        'hero-author.funfact': 'Self-dictated since the age of 14',
+        'hero-links.contact': 'Contact me',
+    },
+} as const
