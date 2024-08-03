13a6612fa6d5815173af36b5be8ef74ae0720c1e
JasonCrk <emerzonkolki@gmail.com>
2024-08-03 07:16:46 -0500

    feat: Improve i18n routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
+    i18n: {
+        defaultLocale: 'es',
+        locales: ['es', 'en'],
+    },
     integrations: [
         icon({
             include: {
